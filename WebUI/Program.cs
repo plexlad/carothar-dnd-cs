23f782ec541bf4396955ba051de9b68236d9c222
plexlad <brock@tylerjsmith.me>
2024-04-17 16:41:38 -0600

    Working with basic page setup and localStorage progress started

--- WebUI/Program.cs
+++ WebUI/Program.cs
@@ -1,10 +1,13 @@
 using WebUI.Components;
+using Blazored.LocalStorage;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+builder.Services.AddBlazoredLocalStorage();
 
 var app = builder.Build();
 
